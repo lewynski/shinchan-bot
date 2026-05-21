c6bdd088552a6de2a567fe0ab423d10979dec88f
Jon Lewyn Tanggaro <131416862+lewynski@users.noreply.github.com>
2026-05-21 20:23:38 +0800

    Create cash.py

---
+++ cogs/cash.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+class CashCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command(
+        name="cash", 
+        aliases=["bal", "balance", "scash"], 
+        description="Check your cash balance."
+    )
+    async def cash(self, ctx: commands.Context):
+        
+        collection = self.bot.db["daily_cooldowns"]
+        user_data = await collection.find_one({"_id": ctx.author.id}) or {}
+        
+        coins = user_data.get("coins", 0)
+        cash_emoji = "<a:cash:1506921225484767282>"
+        demoncat_emoji = "<:demoncat:1506995624879329490>"
+        
+        # Clean, direct text with the demoncat at the end of the sentence
+        text = (
+            f"You currently have {cash_emoji} **{coins:,}** {demoncat_emoji}.\n"
+            f"-# Watch your pockets. The streets aren't always safe."
+        )
+            
+        await ctx.send(content=text)
+
+async def setup(bot):
+    await bot.add_cog(CashCommand(bot))
